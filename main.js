fcbb78993ec2535ce6443f9b752e16f77e79549c
Andriikryl <andreyk557@gmail.com>
2023-05-21 19:05:28 +0300

    feat[add header - html / css with mobiel addaptive ]

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const burger = document?.querySelector("[data-burger]");
+const logo = document?.querySelector(".header__logo");
+
+const nav = document?.querySelector("[data-nav]");
+const body = document.body;
+
+burger?.addEventListener("click", () => {
+  body.classList.toggle("stop-scroll");
+  burger?.classList.toggle("burger--active");
+  nav.classList.toggle("nav--visible");
+  logo.classList.toggle("logo-dark");
+});
